d11e1b62e77bf264061c721adcedb304ccfd4e70
Newton Truong <n3wtn9@gmail.com>
2014-03-24 23:40:53 -0700

    first cut

--- src/magicball.py
+++ src/magicball.py
@@ -1,9 +1,55 @@
 import nltk
+import random
 
-sent = "What doesn't kill you, only makes you stronger"
+def getTagTokens():
+    text_file = open("tagtokens.dat","r")
+    lines = text_file.readlines()
+    text_file.close()
+    index = 0
+    for line in lines:
+       lines[index] = line.strip()
+       index = index + 1
+    return lines
 
-tokens = nltk.word_tokenize(sent)
-tagged = nltk.pos_tag(tokens)
-entities = nltk.chunk.ne_chunk(tagged)
+def getTagLibs():
+    text_file = open("taglibs.dat","r")
+    lines = text_file.readlines()
+    text_file.close()
+    index = 0
+    for line in lines:
+        lines[index] = line.strip()
+        index = index + 1
+    return lines
 
-print entities 
+def main(): 
+    # input
+    sent = "What doesn't kill you, only makes you stronger"
+    tagTokens = getTagTokens()
+    tagLibs = getTagLibs()
+
+    # randomness
+    token_index = random.randrange(0, len(tagTokens), 1)
+    adlib_index = random.randrange(0, len(tagLibs), 1) 
+
+    tokens = nltk.word_tokenize(tagLibs[adlib_index])
+    tagged = nltk.pos_tag(tokens)
+    entities = nltk.chunk.ne_chunk(tagged)
+
+    # valid pos tag replacement
+    valid = 'NN'
+
+    replace_index = -1
+    index = 0
+    for entity in entities:
+        if entity[1] == valid:
+            break
+        replace_index = replace_index + 1
+
+    print "original tag: " + tagLibs[adlib_index]
+    print "token: " + tagTokens[token_index]
+    print entities
+
+    if replace_index != -1:
+        entities[replace_index + 1] = (tagTokens[token_index], valid)
+   
+    return ' '.join([w for w, t in entities.leaves()]) 
