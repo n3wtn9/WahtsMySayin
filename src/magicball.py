7a4ca072cbae02d2ef456bd9b2f24cfdf312d3e4
dave <github@alishabeth.com>
2014-03-25 16:51:14 -0700

    version 2

--- src/magicball.py
+++ src/magicball.py
@@ -1,55 +1,27 @@
-import nltk
 import random
 
-def getTagTokens():
-    text_file = open("tagtokens.dat","r")
-    lines = text_file.readlines()
-    text_file.close()
-    index = 0
-    for line in lines:
-       lines[index] = line.strip()
-       index = index + 1
-    return lines
+import nltk
 
-def getTagLibs():
-    text_file = open("taglibs.dat","r")
-    lines = text_file.readlines()
-    text_file.close()
-    index = 0
-    for line in lines:
-        lines[index] = line.strip()
-        index = index + 1
-    return lines
 
-def shake(): 
+def get_lines(filename):
+    with open(filename, 'r') as f:
+        return [line.strip() for line in f]
+
+
+def shake():
     # input
-    sent = "What doesn't kill you, only makes you stronger"
-    tagTokens = getTagTokens()
-    tagLibs = getTagLibs()
+    tag_tokens = get_lines('tagtokens.dat')
+    tag_libs = get_lines('taglibs.dat')
 
-    # randomness
-    token_index = random.randrange(0, len(tagTokens), 1)
-    adlib_index = random.randrange(0, len(tagLibs), 1) 
+    og_tagline = random.choice(tag_libs)
 
-    tokens = nltk.word_tokenize(tagLibs[adlib_index])
+    tokens = nltk.word_tokenize(og_tagline.lower())
     tagged = nltk.pos_tag(tokens)
     entities = nltk.chunk.ne_chunk(tagged)
 
-    # valid pos tag replacement
-    valid = 'NN'
+    print "original tagline: " + og_tagline
+    return "new tagline:" + ' '.join([w if t != 'NN' else random.choice(tag_tokens) for w, t in entities.leaves()])
 
-    replace_index = -1
-    index = 0
-    for entity in entities:
-        if len(entity) ==2 and entity[1] == valid:
-            break
-        replace_index = replace_index + 1
 
-    print "original tag: " + tagLibs[adlib_index]
-    print "token: " + tagTokens[token_index]
-    print entities
-
-    if replace_index != -1:
-        entities[replace_index + 1] = (tagTokens[token_index], valid)
-   
-    return ' '.join([w for w, t in entities.leaves()]) 
+if __name__ == '__main__':
+    print shake()
